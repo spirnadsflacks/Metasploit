eaabf1825fed4a1d6cac101f3d2090b2ed4940c0
spirnadsflacks <spirnadsflacks@gmail.com>
2017-09-12 02:27:30 -0300

    segundo commit

---
+++ Metasploit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#instalado metasploit 
+echo "SPIRNADS FLACKS By BL4CK"
+echo " Voce Deseja Instalar Metasploit [s/n]";
+read me
+if [ "$me" = "s" ]
+then 
+# instalação iniciando 
+wget https://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run; 
+echo "Instalado com sucesso! SPIRNADS FLACKS By BL4CK"
+else
+# não instalado
+echo "nao instalado!"
+fi
+#by BL4CK 
+
+
+
+ 
